c446fa603ae2fe7bd047d8411e70d89071654b6e
nivrichter <nivrichter@gmail.com>
2022-05-29 11:43:26 +0300

    format

--- backend/seeds.js
+++ backend/seeds.js
@@ -1,68 +1,52 @@
-var seeder = require('mongoose-seed');
-require('./models/User');
-require('./models/Comment');
+var seeder = require("mongoose-seed");
+require("./models/User");
+require("./models/Comment");
 // Connect to MongoDB via Mongoose
-seeder.connect(process.env.MONGODB_URI, function() {
- 
+seeder.connect(process.env.MONGODB_URI, function () {
   // Load Mongoose models
-  seeder.loadModels([
-    'models/Comment.js',
-    'models/Item.js',
-    'models/User.js'
+  seeder.loadModels(["models/Comment.js", "models/Item.js", "models/User.js"]);
 
-  ]);
- 
   // Clear specified collections
-  seeder.clearModels(['Comment', 'Item', 'User'], function() {
- 
+  seeder.clearModels(["Comment", "Item", "User"], function () {
     // Callback to populate DB once collections have been cleared
-    seeder.populateModels(data, function() {
+    seeder.populateModels(data, function () {
       seeder.disconnect();
     });
- 
   });
 });
- 
+
 // Data array containing seed data - documents organized by Model
-let items =[]
-let comments = []
-let users = []
-for(let i=0; i<100; i++) {
+let items = [];
+let comments = [];
+let users = [];
+for (let i = 0; i < 100; i++) {
   const item = {
-      'title': `title-${i}`,
-      'description': 'desc1'
-  }
-  items.push(item)
+    title: `title-${i}`,
+    description: "desc1",
+  };
+  items.push(item);
   const comment = {
-
-    'body': `body-${i}`,
-    
-  }
-  comments.push(comment)
+    body: `body-${i}`,
+  };
+  comments.push(comment);
   const user = {
-    'username': `username${i}`,
-    'email': `a${i}@a.com`
-  }
-  users.push(user)
+    username: `username${i}`,
+    email: `a${i}@a.com`,
+  };
+  users.push(user);
 }
-
 
 var data = [
-    {
-        'model': 'Item',
-        'documents':items
-
-
-    },
-    {
-      'model': 'Comment',
-      'documents':comments
-      
+  {
+    model: "Item",
+    documents: items,
   },
   {
-    'model': 'User',
-    'documents':users
-
-    
-}
+    model: "Comment",
+    documents: comments,
+  },
+  {
+    model: "User",
+    documents: users,
+  },
 ];
